e4222e2d87ca94b02e90f7ec266009590f5da532
Fartash Faghri <fartash.faghri@gmail.com>
2019-05-15 13:20:57 -0400

    cifar100

--- models/__init__.py
+++ models/__init__.py
@@ -23,21 +23,23 @@ def init_model(opt):
             model = models.mnist.SuperSmallMLP(not opt.nodropout)
         # else:
         #     model = models.mnist.MNISTNet()
-    elif opt.dataset == 'cifar10' or opt.dataset == 'svhn':
+    elif (opt.dataset == 'cifar10' or opt.dataset == 'svhn'
+          or opt.dataset == 'cifar100'):
         # model = torch.nn.DataParallel(
         #     models.cifar10.__dict__[opt.arch]())
         # model.cuda()
         if opt.arch == 'cnn':
-            model = models.cifar10.Convnet()
+            model = models.cifar10.Convnet(num_class=opt.num_class)
         elif opt.arch == 'mlp':
-            model = models.cifar10.MLP()
+            model = models.cifar10.MLP(num_class=opt.num_class)
         elif opt.arch.startswith('wrn'):
             depth, widen_factor = map(int, opt.arch[3:].split('-'))
             # model = models.cifar10_wresnet.Wide_ResNet(28, 10, 0.3, 10)
             model = models.cifar10_wresnet2.WideResNet(
                 depth, opt.num_class, widen_factor, 0.3)
         else:
-            model = models.cifar10.__dict__[opt.arch]()
+            model = models.cifar10.__dict__[opt.arch](
+                num_class=opt.num_class)
         model = torch.nn.DataParallel(model)
     elif opt.dataset == 'imagenet':
         model = models.imagenet.Model(opt.arch, opt.pretrained,
