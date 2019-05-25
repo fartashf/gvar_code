2ff31df9d4d953bc33f01b4bbb8bcd929dc9dc8b
Fartash Faghri <fartash.faghri@gmail.com>
2019-05-25 13:57:28 -0400

    move to nuq/code

--- job.sh
+++ job.sh
@@ -5,5 +5,5 @@ job=$3
 
 ssh bolt$machine \
     source /ais/fleet10/faghri/export_p1.sh\; \
-    cd $HOME/nuq/\;\
+    cd $HOME/nuq/code/\;\
     CUDA_VISIBLE_DEVICES=$gpu sh jobs/bolt"$machine"_gpu"$gpu"_job"$job".sh
